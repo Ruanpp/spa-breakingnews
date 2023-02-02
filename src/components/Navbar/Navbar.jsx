77c99309ea08915cabd0dab02444f2f4fb7de71e
Ruanpp <ruanpablosilvap07@gmail.com>
2023-02-02 10:15:04 -0300

    styled components and global styled

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import logo from "../../images/LogoBN.png";
-import "./Navbar.css"
+import { Button, ImageLogo, InputSpace, Nav } from "./NavbarStyled";
 
 export function Navbar() {
   return (
     <>
-      <nav>
-        <div className="input-search-space">
-        <i className="bi bi-search"></i>
-            <input type="text" placeholder="Pesquise por um título" />
-        </div>
+      <Nav>
+        <InputSpace>
+          <i className="bi bi-search"></i>
+          <input type="text" placeholder="Pesquise por um título" />
+        </InputSpace>
 
-        <img src={logo} all="Logo do Breaking News" />
+        <ImageLogo src={logo} alt="Logo do Breaking News" />
 
-        <button>Entrar</button>
-      </nav>
+        <Button>Entrar</Button>
+      </Nav>
     </>
   );
 }
+
